6c29a53ddf7be478c50f74a2eeaf7c6a36b06870
mehdithe <mehdi.cheracher@gmail.com>
2017-12-31 18:49:34 +0000

    first commit

---
+++ api/cpdash/src/main/java/com/macm/cpdash/CpdashApplication.java
@@ -0,0 +1,12 @@
+package com.macm.cpdash;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CpdashApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CpdashApplication.class, args);
+	}
+}
