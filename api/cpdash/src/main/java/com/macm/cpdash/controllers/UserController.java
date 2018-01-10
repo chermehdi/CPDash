0c6802ad74abfcd53e9ab693484703b5386bdf9b
mehdithe <mehdi.cheracher@gmail.com>
2018-01-10 20:58:34 +0000

    added validity route

--- api/cpdash/src/main/java/com/macm/cpdash/controllers/UserController.java
+++ api/cpdash/src/main/java/com/macm/cpdash/controllers/UserController.java
@@ -7,15 +7,10 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
-import org.springframework.security.core.context.SecurityContextHolder;
-import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-
-import java.security.Principal;
 
 @RestController
 @RequestMapping("/api/users")
@@ -23,12 +18,12 @@ public class UserController {
 
     private final UserRepository userRepository;
 
+    private Logger log = LoggerFactory.getLogger(getClass());
+
     @Autowired
     public UserController(UserRepository userRepository) {
         this.userRepository = userRepository;
     }
-
-    Logger log = LoggerFactory.getLogger(getClass());
 
     @GetMapping("/me")
     public ResponseEntity<?> currentUser(Authentication auth) {
@@ -37,5 +32,18 @@ public class UserController {
         log.info("current email " + currentUserEmail);
         UserEntity currentUser = userRepository.findByEmail(currentUserEmail);
         return ResponseEntity.ok(currentUser);
+    }
+
+    /**
+     * check if the user sending the request is logged in, i.e the token is not tampered with
+     * returns 200 status if their is an authentication in the security context, 400 otherwise
+     */
+    @GetMapping("/validity")
+    public ResponseEntity<?> checkTokenValidity(Authentication auth) {
+        JwtUser userDetails = (JwtUser) auth.getPrincipal();
+        if (userDetails == null) {
+            return ResponseEntity.badRequest().build();
+        }
+        return ResponseEntity.ok().build();
     }
 }
