9aacd90861a783c289e6e22995f7200aed6ff88f
CORP\aeroui <kapi.abdel@gmail.com>
2018-01-17 12:26:35 +0000

    Added Problems Controller (Empty)

---
+++ api/cpdash/src/main/java/com/macm/cpdash/controllers/ProblemsController.java
@@ -0,0 +1,12 @@
+package com.macm.cpdash.controllers;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/problem")
+public class ProblemsController {
+
+	
+	
+}
--- api/cpdash/src/main/java/com/macm/cpdash/repositories/SheetrRepository.java
+++
@@ -1,9 +0,0 @@
-package com.macm.cpdash.repositories;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.macm.cpdash.domain.entities.SheetEntity;
-
-public interface SheetrRepository extends JpaRepository<SheetEntity, Long>{
-	
-}
